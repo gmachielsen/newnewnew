71ab008e11648a860bf2fc1961cc5e9fa76805c4
Gijs Machielsen <gijsmachielsen@iMac-van-Gijs.local>
2021-07-08 17:00:56 +0200

    initial commit

--- client/pages/instructor/course/edit/[slug].js
+++ client/pages/instructor/course/edit/[slug].js
@@ -27,11 +27,12 @@ const CourseEdit = () => {
   const [preview, setPreview] = useState("");
   const [uploadButtonText, setUploadButtonText] = useState("Upload Image");
 
-
   // state for lessons update
   const [visible, setVisible] = useState(false);
   const [current, setCurrent] = useState({});
-  const [uploadVideoButtonText, setUploadVideoButtonText] = useState('Upload Video');
+  const [uploadVideoButtonText, setUploadVideoButtonText] = useState(
+    "Upload Video"
+  );
   const [progress, setProgress] = useState(0);
   const [uploading, setUploading] = useState(false);
 
@@ -40,14 +41,15 @@ const CourseEdit = () => {
   const { slug } = router.query;
 
   useEffect(() => {
-      loadCourse()
-  }, [slug])
+    loadCourse();
+  }, [slug]);
 
   const loadCourse = async () => {
-      const {data} = await axios.get(`/api/course/${slug}`);
-      if (data) setValues(data);
-      if (data && data.image) setImage(data.image);
-  }
+    const { data } = await axios.get(`/api/course/${slug}`);
+    console.log(data);
+    if (data) setValues(data);
+    if (data && data.image) setImage(data.image);
+  };
 
   const handleChange = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
@@ -95,26 +97,25 @@ const CourseEdit = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-        // console.log(values);
-        const {data} = await axios.put(`/api/course/${slug}`, {
-            ...values,
-            image,
-        });
-        toast("Course updated!");
-        // router.push("/instructor");
+      // console.log(values);
+      const { data } = await axios.put(`/api/course/${slug}`, {
+        ...values,
+        image,
+      });
+      toast("Course updated!");
+      // router.push("/instructor");
     } catch (err) {
-        toast(err.response.data);
+      toast(err.response.data);
     }
-
   };
 
   const handleDrag = (e, index) => {
-    //   console.log("ON DRAG => ", index);
-    e.dataTransfer.setData('itemIndex', index);
+    // console.log("ON DRAG => ", index);
+    e.dataTransfer.setData("itemIndex", index);
   };
 
   const handleDrop = async (e, index) => {
-    //   console.log("ON DROP => ", index);
+    // console.log("ON DROP => ", index);
 
     const movingItemIndex = e.dataTransfer.getData("itemIndex");
     const targetItemIndex = index;
@@ -125,18 +126,16 @@ const CourseEdit = () => {
     allLessons.splice(targetItemIndex, 0, movingItem); // push item after target item index
 
     setValues({ ...values, lessons: [...allLessons] });
-
+    // save the new lessons order in db
     const { data } = await axios.put(`/api/course/${slug}`, {
-        ...values,
-        image,
+      ...values,
+      image,
     });
-    console.log('Lessons rearranged res => ', data);
-    toast('Lessons rearranged successfully');
+    // console.log("LESSONS REARRANGED RES => ", data);
+    toast("Lessons rearranged successfully");
   };
 
   const handleDelete = async (index) => {
-    // console.log("delete lesson");
-    // return;
     const answer = window.confirm("Are you sure you want to delete?");
     if (!answer) return;
     let allLessons = values.lessons;
@@ -145,7 +144,7 @@ const CourseEdit = () => {
     setValues({ ...values, lessons: allLessons });
     // send request to server
     const { data } = await axios.put(`/api/course/${slug}/${removed[0]._id}`);
-    console.log('LESSON DELETED =>', data);
+    console.log("LESSON DELETED =>", data);
   };
 
   /**
@@ -153,11 +152,46 @@ const CourseEdit = () => {
    */
 
   const handleVideo = () => {
-    console.log("handle video");
-  }
+    // remove privious video
+    if(current.video && current.video.Location) {
+      const res = await axios.post(`/api/course/video-remove/${values.instructor._id}`, 
+      current.video
+      );
+      console.log("REMOVED ====>", res);
+    }
+    // upload
+    const file = e.target.files[0]
+    setUploadVideoButtonText(file.name);
+    setUploading(true);
+    // send video as form data
+    const videoData = new FormData()
+    videoData.append('video', file);
+    videoData.append('courseId', values._id);
+    // save progress bar and send video as form data to backend
+    const { data } = await axios.post(
+      `/api/course/video-upload/${values.instructor._id}`,
+      videoData,
+      {
+        onUploadProgress: (e) => 
+          setProgress(Math.round((100 * e.loaded) / e.total)),
+      }
+    );
+    console.log(data);
+    setCurrent({ ...current, video: data });
+    setUploading(false);
+  };
 
-  const handleUpdateLesson = () => {
-    console.log("handle update lesson");
+  const handleUpdateLesson = async (e) => {
+    // console.log("handle update lesson");
+    e.preventDefault();
+    const { data } = await axios.put(
+      `/api/course/lesson/${slug}/${current._id}`,
+      current
+    );
+    setUploadVideoButtonText("Upload Video")
+    setVisible(false)
+    toast('Lesson updated');
+    setCourse(data);
   };
 
   return (
@@ -167,9 +201,9 @@ const CourseEdit = () => {
       <div className="pt-3 pb-3">
         <CourseCreateForm
           handleSubmit={handleSubmit}
+          handleImageRemove={handleImageRemove}
           handleImage={handleImage}
           handleChange={handleChange}
-          handleImageRemove={handleImageRemove}
           values={values}
           setValues={setValues}
           preview={preview}
@@ -180,55 +214,59 @@ const CourseEdit = () => {
       {/* <pre>{JSON.stringify(values, null, 4)}</pre>
       <hr />
       <pre>{JSON.stringify(image, null, 4)}</pre> */}
-<hr></hr>
-        <div className="row pb-5">
-            <div className="col lesson-list">
-            <h4>
-                {values && values.lessons && values.lessons.length} Lessons
-            </h4>
-            <List
-                onDragOver={(e) => e.preventDefault()}
-                itemLayout="horizontal"
-                dataSource={values && values.lessons}
-                renderItem={(item, index) => (
-                    <Item
-                        draggable 
-                        onDragStart={(e) => handleDrag(e, index)}
-                        onDrop={(e) => handleDrop(e, index)}
-                    >
-                    <Item.Meta
-                    onClick={() => {
-                      setVisible(true)
-                      setCurrent(item);
-                    }}
-                    avatar={<Avatar>{index + 1}</Avatar>}
-                    title={item.title}
-                    ></Item.Meta>
-                    <DeleteOutlined onClick={() => handleDelete(index)} className="text-danger float-right" />
-                </Item>
-                )}
-            ></List>
-            </div>
-        </div>
 
-        <Modal
-          title="Update lesson"
-          centered 
-          visible={visible}
-          onCancel={() => setVisible(false)}
-          footer={null} 
-        >
-          <UpdateLessonForm 
-            current={current} 
-            setCurrent={setCurrent}
-            handleVideo={handleVideo}
-            handleUpdateLesson={handleUpdateLesson}
-            uploadVideoButtonText={uploadVideoButtonText}
-            progress={progress}
-            uploading={uploading}
-          />
-          <pre>{JSON.stringify(current, null, 4)}</pre>
-        </Modal>
+      <hr />
+
+      <div className="row pb-5">
+        <div className="col lesson-list">
+          <h4>{values && values.lessons && values.lessons.length} Lessons</h4>
+          <List
+            onDragOver={(e) => e.preventDefault()}
+            itemLayout="horizontal"
+            dataSource={values && values.lessons}
+            renderItem={(item, index) => (
+              <Item
+                draggable
+                onDragStart={(e) => handleDrag(e, index)}
+                onDrop={(e) => handleDrop(e, index)}
+              >
+                <Item.Meta
+                  onClick={() => {
+                    setVisible(true);
+                    setCurrent(item);
+                  }}
+                  avatar={<Avatar>{index + 1}</Avatar>}
+                  title={item.title}
+                ></Item.Meta>
+
+                <DeleteOutlined
+                  onClick={() => handleDelete(index)}
+                  className="text-danger float-right"
+                />
+              </Item>
+            )}
+          ></List>
+        </div>
+      </div>
+
+      <Modal
+        title="Update lesson"
+        centered
+        visible={visible}
+        onCancel={() => setVisible(false)}
+        footer={null}
+      >
+        <UpdateLessonForm
+          current={current}
+          setCurrent={setCurrent}
+          handleVideo={handleVideo}
+          handleUpdateLesson={handleUpdateLesson}
+          uploadVideoButtonText={uploadVideoButtonText}
+          progress={progress}
+          uploading={uploading}
+        />
+        {/* <pre>{JSON.stringify(current, null, 4)}</pre> */}
+      </Modal>
     </InstructorRoute>
   );
 };
--- server/controllers/course.js
+++ server/controllers/course.js
@@ -235,6 +235,36 @@ exports.removeLesson = async (req, res) => {
   const deletedCourse = await Course.findByIdAndUpdate(course._id, {
     $pull: { lessons: { _id: lessonId }},
   }).exec();
-
   res.json({ ok: true });
+};
+
+
+exports.updateLesson = async (req, res) => {
+  console.log("UPDATE LESSON", req.body);
+  try {  
+    const { slug } = req.params;
+    const { _id, title, content, video, free_preview } = req.body;
+    const course = await Course.findOne({ slug }).select("instructor").exec();
+
+    if (course.instructor._id != req.user._id) {
+      return res.status(400).send("Unauthorized");
+    }
+
+    const updated = await Course.updateOne({ "lessons._id" : _id },
+    {
+      $set: {
+        "lessons.$.title": title,
+        "lessons.$.content": content,
+        "lessons.$.video": video,
+        "lessons.$.free_preview": free_preview,
+      }
+    },
+    { new: true }
+    ).exec();
+    console.log("updated", updated);
+    res.json({ ok: true });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send("Update lesson failed");
+  }
 };
